6a7682d812b5529ef04da62b10827baeec315d30
Martinmateucci <142510601+Martinmateucci@users.noreply.github.com>
2023-08-17 10:23:02 -0300

    Add files via upload

---
+++ Martinmateucci.java
@@ -0,0 +1,21 @@
+//Paquete que contiene la clase
+package Clase_1;  
+
+//Se crea la clase
+public class Martinmateucci{
+
+ //se crea un metodo   
+public static void main(String[] args) {
+    
+    //Variable de tipo entero
+    int numero1 = 21;
+    //Variable del numero float
+    float numero2 = numero1;
+
+    //Imprime por pantalla el valor de numero1
+    System.out.println(numero1);
+    //Imprime por pantalle el valor de numero2
+    System.out.println(numero2);
+
+}
+}
